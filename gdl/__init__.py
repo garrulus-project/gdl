5d4043ca4361c738367fa0e4945846164da22dd2
Mohammad Wasil <mohammad.wasil@h-brs.de>
2025-06-18 08:16:33 +0000

    bump version

--- gdl/__init__.py
+++ gdl/__init__.py
@@ -1,5 +1,4 @@
-"""GDL: Garrulus dataset library for creating data samplers, transforms, and pre-trained models for geospatial data.
-"""
+"""GDL: Garrulus dataset library for creating data samplers, transforms, and pre-trained models for geospatial data."""
 
 __author__ = 'Mohammad Wasil'
-__version__ = '0.1.0.dev0'
+__version__ = '0.1.1.dev0'
